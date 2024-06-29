04d5af17a385f2202a5dd995a30db2e508b5e08c
Alejandro Martinez <janusreal@gmail.com>
2024-06-29 13:52:53 -0400

    Ejercicio para probar cosas

---
+++ config.example.php
@@ -0,0 +1,3 @@
+<?php
+// config.example.php
+define('API_KEY', 'tu_api_key_aqui');
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require('config.php')
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio de Pruebas</title>
+</head>
+<body>
+    <?php echo PASSWORD; ?>
+</body>
+</html>
